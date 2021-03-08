f95039a79903d990d82cf461a076da1491250172
Ehren-XtremeApp <ehren@xtremeapp.com.tw>
2021-03-09 01:24:14 +0800

    [index/main] add and link to main page, done with part of side bar

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// sideBar Man下拉選單
+function openManOption(){
+  let option = document.getElementById('man-option');
+  option.classList.toggle('option-open');
+};
+
+// sideBar Woman下拉選單
+function openWomanOption(){
+  let option = document.getElementById('woman-option');
+  option.classList.toggle('option-open');
+};
